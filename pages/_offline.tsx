d116e73b5a3a97e7d4c5d28f57e8f9402193ba35
Josh Medeski <josh.medeski@gmail.com>
2021-09-04 17:46:42 -0500

    feat: add offline page

---
+++ pages/_offline.tsx
@@ -0,0 +1,8 @@
+import CollectionGrid from "@vc/features/collection/CollectionGrid";
+import React from "react";
+
+const OfflinePage: React.FC = () => {
+  return <CollectionGrid />;
+};
+
+export default OfflinePage;
