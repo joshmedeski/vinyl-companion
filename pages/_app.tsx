d3392e93422681e94a3a7369294b9886e10c9094
Josh Medeski <josh.medeski@gmail.com>
2021-09-04 17:20:59 -0500

    feat: make pwa and add icons and manifest

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,11 +5,13 @@ import type { AppProps } from "next/app";
 import { OfflineDbProvider } from "@vc/features/offline-db/OfflineDbContext";
 import { DiscogsProvider } from "@vc/features/discogs/DiscogsContext";
 import Footer from "@vc/ui/footer";
+import Manifest from "@vc/ui/manifest";
 
 function VinylCompanionApp({ Component, pageProps }: AppProps) {
   return (
     <OfflineDbProvider>
       <DiscogsProvider>
+        <Manifest />
         <main className="min-h-screen pb-20">
           <Component {...pageProps} />
         </main>
