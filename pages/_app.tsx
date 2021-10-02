f4e89425d47a89fc25ecac894ee4b7a53132f6ce
Josh Medeski <josh.medeski@gmail.com>
2021-10-01 22:48:47 -0500

    fix: title tag to "Vinyl Companions"

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,31 +6,37 @@ import { OfflineDbProvider } from "@vc/features/offline-db/OfflineDbContext";
 import { DiscogsProvider } from "@vc/features/discogs/DiscogsContext";
 import Manifest from "@vc/ui/manifest";
 import Settings from "@vc/ui/settings";
+import Head from "next/head";
 
 function VinylCompanionApp({ Component, pageProps }: AppProps) {
   return (
-    <OfflineDbProvider>
-      <DiscogsProvider>
-        <Manifest />
-        <div className="dark dark:bg-black">
-          <header className="p-4">
-            <div className="flex align-center justify-between gap-4">
-              <h1 className="dark:text-white font-extrabold uppercase tracking-wider">
-                Vinyl Companion
-              </h1>
+    <>
+      <Head>
+        <title>Vinyl Companion</title>
+      </Head>
+      <OfflineDbProvider>
+        <DiscogsProvider>
+          <Manifest />
+          <div className="dark dark:bg-black">
+            <header className="p-4">
+              <div className="flex align-center justify-between gap-4">
+                <h1 className="dark:text-white font-extrabold uppercase tracking-wider">
+                  Vinyl Companion
+                </h1>
 
-              <div className="flex gap-6">
-                <Settings />
+                <div className="flex gap-6">
+                  <Settings />
+                </div>
               </div>
-            </div>
-          </header>
+            </header>
 
-          <main className="min-h-screen pb-20">
-            <Component {...pageProps} />
-          </main>
-        </div>
-      </DiscogsProvider>
-    </OfflineDbProvider>
+            <main className="min-h-screen pb-20">
+              <Component {...pageProps} />
+            </main>
+          </div>
+        </DiscogsProvider>
+      </OfflineDbProvider>
+    </>
   );
 }
 export default VinylCompanionApp;
